1eff7c56130fbe97b3f5b6b63119edd8326cf268
irilian <79520018+irilian@users.noreply.github.com>
2021-03-07 18:12:32 +0300

    Update kod.js

--- kod.js
+++ kod.js
@@ -9,3 +9,8 @@ prefix: "Lütfen prefixinizi yazınız."
 let p = client.irilian.prefix;
             if(message.content.startsWith(`<@!${client.user.id}>`)) p = `<@!${client.user.id}> `
             if(!message.content.startsWith(p)) return; //Eklenmezse, tüm mesajlarda dener.
+
+
+//Prefix yeri ne mi olabilir? 🤔
+if (!message.content.startsWith(ayarlar.prefix)) return;
+//Bu şekilde olabileceğini düşünmekteyim. Her altyapıya göre değişmektedir. Herhangi bir sorununuz olduğu zaman; DC: sadece irilian#2833. İyi kullanımlar!
